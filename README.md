511371bc5e25257ead7823c9f4a4a6655600a3cd
Richard Bann <richardbann@gmail.com>
2021-10-23 21:35:06 +0200

    initial

---
+++ README.md
@@ -0,0 +1,15 @@
+## Install Dev Env (ubuntu 20.04)
+
+- `apt install python3.9`
+- `python3.9 -m venv .venv`
+- `curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3.9 -`
+- `poetry install`
+- `curl -sL https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.0/install.sh -o install_nvm.sh` (delete it later)
+- `bash install_nvm.sh`
+- `nvm install --lts`
+- `npm install -g yarn`
+- `yarn set version berry` (in project dir)
+
+install docker-compose
+zsh ?
+oh-my-zsh setup (zsh-nvm)
---
+++ pages/index.js
@@ -0,0 +1,81 @@
+import Head from "next/head";
+
+function HomePage() {
+  return (
+    <>
+      <Head>
+        <title>Welcome</title>
+      </Head>
+      <div>
+        <article className="prose">
+          <h1>Welcome to Tailwind&apos;s Typography and Many More</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
+            scelerisque lacus est, quis tincidunt ex condimentum quis. Sed a
+            nibh non nibh mollis finibus eget ac odio. Suspendisse semper
+            fringilla bibendum. Vivamus eget mauris arcu. Nulla rutrum arcu et
+            nisi tincidunt, ac pellentesque lorem accumsan. Duis ut urna dui. In
+            eget auctor enim. Morbi fermentum porta mattis. Maecenas dictum ut
+            lacus molestie egestas. Nunc sit amet augue leo. Aliquam commodo
+            volutpat risus eget dignissim. Vestibulum facilisis nibh nec
+            pulvinar tempor. Curabitur ultrices varius lorem, id tempor ipsum
+            commodo non. Nullam arcu sem, tincidunt nec felis a, egestas
+            tincidunt libero. Morbi ut dolor ac risus accumsan tincidunt ut a
+            nibh. Curabitur ante augue, blandit non sapien id, ultrices
+            tristique lorem.
+          </p>
+          <p>
+            Nam tristique velit quis ex vestibulum, suscipit ullamcorper nunc
+            hendrerit. Nullam rutrum est vel ornare eleifend. Maecenas nibh
+            lorem, gravida sit amet libero quis, aliquet fermentum lorem. Duis
+            venenatis consequat mauris sed mollis. Vivamus laoreet sodales
+            pellentesque. Duis in urna vitae ex consequat facilisis. Vivamus
+            vehicula fringilla ipsum, at sagittis ex gravida non. Nulla
+            efficitur scelerisque nunc in lobortis. Nullam sollicitudin arcu in
+            mi euismod, at tincidunt purus rhoncus. Pellentesque bibendum
+            lobortis magna, vitae lacinia risus pharetra id. Aliquam sed
+            sollicitudin augue.
+          </p>
+          <p>
+            Sed a metus suscipit nisl sodales maximus. Vivamus vel libero
+            varius, efficitur nulla congue, rhoncus lectus. In posuere, orci
+            eget rutrum tempus, est eros faucibus odio, nec fringilla libero
+            lacus nec lectus. Donec tellus ipsum, aliquam in lorem in, semper
+            luctus odio. Nulla porta libero a ultricies lobortis. Donec varius,
+            risus at dictum suscipit, ipsum augue porta tortor, et mollis est
+            augue ut lectus. Fusce eu lacus dapibus purus vestibulum tempus.
+            Etiam aliquet vel lectus a consequat. Praesent vel ante sit amet
+            nibh pharetra semper sit amet sit amet ipsum. Maecenas laoreet
+            sollicitudin ex, vel semper augue laoreet sed. Sed elementum magna
+            vestibulum, mattis tortor at, porttitor nibh. Phasellus nisi erat,
+            pulvinar sed vestibulum vel, dictum eget nunc. Aliquam viverra diam
+            placerat, ornare elit nec, tristique turpis. Suspendisse leo dui,
+            commodo at congue et, egestas nec nunc.
+          </p>
+          <p>
+            Fusce eget diam ut libero efficitur cursus vel quis velit. Ut
+            lacinia rhoncus luctus. Suspendisse viverra ex quis ligula sagittis
+            placerat. Aenean egestas justo ac metus lobortis euismod.
+            Suspendisse potenti. Vivamus facilisis velit urna. Pellentesque
+            habitant morbi tristique senectus et netus et malesuada fames ac
+            turpis egestas. Cras ac felis et metus blandit mattis ut eu diam.
+            Class aptent taciti sociosqu ad litora torquent per conubia nostra,
+            per inceptos himenaeos. Donec pharetra odio in lacus tincidunt
+            tincidunt ac vitae tortor. Aenean maximus urna lacus, et hendrerit
+            mauris laoreet vel. Aliquam efficitur ut mauris a scelerisque.
+          </p>
+          <p>
+            Aliquam sit amet sapien et tellus dapibus tempus. Integer nulla
+            nunc, egestas eget sapien at, dignissim lacinia diam. Integer
+            egestas at sem at ornare. Nullam dapibus eu risus a mattis. Nam quis
+            leo eget nisl auctor tempus quis at nibh. Donec lorem ante, maximus
+            in bibendum vitae, egestas sit amet tortor. Ut sit amet convallis
+            eros. Curabitur at condimentum leo. Aenean ac hendrerit nisi.
+          </p>
+        </article>
+      </div>
+    </>
+  );
+}
+
+export default HomePage;
